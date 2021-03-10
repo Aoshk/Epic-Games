8bb4de4a01b3aa3f9b513dbc877e43c1f61c5ba8
Sok4h <55671660+Sok4h@users.noreply.github.com>
2021-03-10 07:23:58 -0500

    galeria funcional

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,19 @@
 let botonMenu = document.querySelector(".imgMenuMobile");
 let menuMobile = document.querySelector(".divMenuMobile")
 let closeMenu = document.querySelector(".closeMenu");
+let bannerHitman = document.querySelector(".imageBanner");
+let images = document.querySelectorAll(".images");
+
+document.addEventListener('click', function(e){
+
+    let target = e.target;
+    //console.log(e.target.className)
+    if(target.className=="images"){
+
+        bannerHitman.src=target.src;
+    }
+  });
+
 
 botonMenu.addEventListener("click", ()=>{
 
