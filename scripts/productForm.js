5b859d5725acc1b72d0a2362610c4eb0c0f75a14
Sok4h <55671660+Sok4h@users.noreply.github.com>
2021-04-07 19:33:21 -0500

    Product form design updated + upload to firestore

--- scripts/productForm.js
+++ scripts/productForm.js
@@ -1,6 +1,7 @@
 const productForm = document.querySelector(".productForm");
+const db = firebase.firestore();
 
-
+console.log(db)
 productForm.addEventListener("submit", (event)=>{
 
     event.preventDefault();
@@ -8,13 +9,15 @@ productForm.addEventListener("submit", (event)=>{
     const product = {
 
     name: productForm.name.value,
-    price: productForm.price.value,
+    price: parseFloat(productForm.price.value),
     genre: productForm.genre.value,
     description: productForm.description.value,
-    metacritic: productForm.metacritic.value,
+    metacritic: productForm.metacritic.value
     }
 
+    
     console.log(product);
+    db.collection("products").add(product);
 
 })
 
