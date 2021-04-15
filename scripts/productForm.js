d3673402a558a3438820776894451d73e9f9cc32
Sok4h <55671660+Sok4h@users.noreply.github.com>
2021-04-15 15:59:50 -0500

    image form preview

--- scripts/productForm.js
+++ scripts/productForm.js
@@ -1,23 +1,43 @@
 const productForm = document.querySelector(".productForm");
 const db = firebase.firestore();
+const storage = firebase.storage();
+const productImg = document.querySelector(".productFormImg");
 
-console.log(db)
+let storageRef= firebase.storage().ref();
+
+console.log(productForm)
+
+//actualiza la vista previa de la imagen
+productForm.image.addEventListener("change",()=>{
+
+    var reader = new FileReader();
+
+    reader.onload = function(e){
+
+        productImg.setAttribute("src",e.target.result);
+    }
+
+    reader.readAsDataURL(productForm.image.files[0]);
+});
+
+
+
+//evento de subir el producto
 productForm.addEventListener("submit", (event)=>{
 
     event.preventDefault();
 
-    const product = {
-
-    name: productForm.name.value,
-    price: parseFloat(productForm.price.value),
-    genre: productForm.genre.value,
-    description: productForm.description.value,
-    metacritic: productForm.metacritic.value
-    }
+    // const product = {
+    // name: productForm.name.value,
+    // price: parseFloat(productForm.price.value),
+    // genre: productForm.genre.value,
+    // description: productForm.description.value,
+    // metacritic: productForm.metacritic.value
+    // }
 
     
-    console.log(product);
-    db.collection("products").add(product);
+    console.log(productForm.image.files);
+    //db.collection("products").add(product);
 
 })
 
